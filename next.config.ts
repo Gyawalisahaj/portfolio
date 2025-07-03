4bb989c48decac858192972ac1ec9a5f8ffeb571
Gyawalisahaj <sahajgnawali@gmail.com>
2025-07-03 17:08:05 +0545

    Static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production';
+// next.config.js
+const isGithubPages = process.env.NODE_ENV === 'production';
+
+const repo = 'portfolio'; // your repo name
 
 module.exports = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
-  images: {
-    unoptimized: true,
-  },
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
